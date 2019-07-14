a59ee167a178d53133b4e74ec73e6fc8ebcc5c1c
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-13 22:57:41 -0700

    Updates

---
+++ precache-manifest.2295052b6d461ae44a20a022627c23d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f456cc60afa3a7b116b565aa561a201",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4b45c3827bee187d20b6",
+    "url": "/clicky-game/static/css/main.2c7f60be.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "4b45c3827bee187d20b6",
+    "url": "/clicky-game/static/js/main.08103b37.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "e0d4391fd92dc7b9a594d59d2d4a4226",
+    "url": "/clicky-game/static/media/yosemite.e0d4391f.jpg"
+  }
+]);
